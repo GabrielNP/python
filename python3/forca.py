a565354746b9d209f36718ca40bf1a3877c35efe
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-19 20:14:59 -0300

    Starting develop hangman game

--- python3/forca.py
+++ python3/forca.py
@@ -4,6 +4,28 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana";
+    palavra_secreta.upper()
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? R: ")
+        # Elimina os espaços em branco
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra \"{}\" na posição {}".format(letra,index+1))
+            index = index + 1
+
+        print("\nJogando...")
+
+
 
     print("\n\n*********************************")
     print("Fim do jogo")
