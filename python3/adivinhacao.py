3881fbe239b5bc14c3f0d8918160df38c3e434db
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-19 15:53:49 -0300

    Improving game

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -4,15 +4,37 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_tentativas = 3
+tentativas_restantes = total_tentativas
+rodada = 1
 
-chute = input("\nAdvinhe o seu número: ")
-print("\nVocê digitou: ", chute)
+while (tentativas_restantes > 0):
+    
+    print("\n\n **** Rodada", rodada, "de",total_tentativas, "****")
+    chute = input("\nAdvinhe o seu número: ")
+    print("\nVocê digitou: ", chute)
 
-chute = int(chute)
+    chute = int(chute)
 
-if chute == numero_secreto:
-    print("\nVOCÊ ADIVINHOU! PARABÉNS!")
-else:
-    print("\nVOCÊ ERROU!")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
+    if (acertou):
+        print("\nVOCÊ ADIVINHOU! PARABÉNS!")
+        break
+    else:
+        print("\nVOCÊ ERROU!")
+        if (maior):
+            print("O seu chute foi MAIOR que o número secreto!")
+        elif (menor):
+            print("O seu chute foi MENOR do que o número secreto!")
+        
+    rodada = rodada + 1
+    tentativas_restantes = tentativas_restantes -1
+    print("\nTentativas restantes: ",tentativas_restantes)
+
+
+print("*********************************")
 print("\n\nFim do jogo")
+print("*********************************")
