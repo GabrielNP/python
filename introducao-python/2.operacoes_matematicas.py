345f99da0362a3a0f2577c809cc758b2aad24bcc
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-19 17:58:13 -0300

    Integer division

--- introducao-python/2.operacoes_matematicas.py
+++ introducao-python/2.operacoes_matematicas.py
@@ -1,10 +1,18 @@
 print(2 + 2)
 print(2 * 2)
-print(2 / 2)
 print(2 - 2)
+
+# Divisão
+print(2 / 2)
+
+# Integer division
+print(2//2)
 
 # Exponenciação
 print(2 ** 2)
 
 # Módulo
 print(10 % 3)
+
+# Arredondar
+print(round(4.5))
