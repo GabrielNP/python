658405e983767f0f0acffc25e9c9b3add4a81044
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-19 22:27:45 -0300

    Converting list <=> tuple

--- python-iniciantes/4.tuplas.py
+++ python-iniciantes/4.tuplas.py
@@ -2,7 +2,7 @@
 # Sequências imutáveis = Tuplas, range, string
 
 dias = ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
->>> type(dias)
+type(dias)
 
 tuplas=("gabriel","osvaldo","tadeu")
 tuplas
@@ -19,9 +19,34 @@ tuplas*5
 4 in tuplas
 "tadeu" in tuplas
 
+# Array/lista de tuplas
+p1 = (1,2)
+p2 = (2,3)
+p3 = (3,4)
+coordenadas = [p1, p2, p3]
 
-#  Converter array em tupla
+person1 = ("Fulano", 39)
+person2 = ("Beltrano", 25)
+person3 = ("Ciclano", 42)
+people = [person1, person2, person3]
+people[1][0]
+
+
+#  Converter LISTA em TUPLA
 lista = [1,2,"gabriel"]
 lista
 tuplas2 = tuple(lista)
 tuplas2
+
+linhas = []
+type(linhas)
+linhas.append("linha 1")
+linhas.append("linha 2")
+linhas.append("linha 3")
+linhas_tuple = tuple(linhas)
+type(linhas_tuple)
+
+# Converter TUPLA em LISTA
+linhas_list = list(linhas_tuple)
+type(linhas_list)
+print(linhas_list)
