65d6e467dca74a93ca66bd4bed29478577cef7db
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-21 14:27:33 -0300

    Class, objects, constructor function, address and reference, attributes

--- oo/README.md
+++ oo/README.md
@@ -1,3 +1,11 @@
 Estudo baseado no curso https://cursos.alura.com.br/course/python-3-intro-orientacao-objetos
 
-A ideia central do paradigma da Orientação a Objetos: dado e funcionalidade (comportamentos) andam juntos.
+`A ideia central do paradigma da Orientação a Objetos: dado e funcionalidade (comportamentos) andam juntos.`
+
+`Em Orientação a Objetos, as variáveis são denominadas referências e Atributos são conteúdo da classe, as características.`
+
+# Função construtora
+```
+def __init__(self):
+self é a referência que sabe encontrar o objeto construído em memória. 
+```
--- oo/conta.py
+++ oo/conta.py
@@ -1,12 +1,8 @@
-def cria_conta(numero, titular, saldo, limite):
-    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
-    return conta
+class Conta:
 
-def deposita(conta, valor):
-    conta["saldo"] += valor
-
-def saca(conta, valor):
-    conta["saldo"] -= valor
-
-def extrato(conta):
-    print("Saldo {}".format(conta["saldo"]))
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
