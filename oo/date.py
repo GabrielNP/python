18b6b9f49173074494028ca56e77a619f2572e59
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-22 00:50:12 -0300

    Improving Date class

--- oo/date.py
+++ oo/date.py
@@ -1,10 +1,10 @@
 class Date:
 
-    def __init__(self, day, month, year):
-        self.__day = day
-        self.__month = month
-        self.__year = year
+    def __init__(self):
+        from datetime import date
+        self.__day = date.today().day
+        self.__month = date.today().month
+        self.__year = date.today().year
 
     def format_date(self):
          print(self.__day, self.__month, self.__year, sep='/')
-        
