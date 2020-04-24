5fdab5accae1d3059df78b7555f27380aaa6dbdf
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-23 21:26:15 -0300

    Only one underscore to solve name mangling problem

--- oo_paradigm/modelos.py
+++ oo_paradigm/modelos.py
@@ -1,39 +1,39 @@
 class Programa:
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     def dar_likes(self):
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome
+        self._nome = nome
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
     
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
+        self._likes = 0
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
