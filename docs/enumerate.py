befc49710a6ca1c48382724f32d4850f40e3693c
gabrielnp <gabrielnp.2010@gmail.com>
2020-05-27 21:34:52 -0300

    Enumerated

--- docs/enumerate.py
+++ docs/enumerate.py
@@ -3,9 +3,35 @@ lista = ["abacate", "bola", "cachorro"]
 for i in range(len(lista)):
     print(i, lista[i])
 
-
-# Enumerate
 print("\n\nUsando função enumerate")
 
-for i,nome in enumerate(lista):
-    print(i,nome)
+for indice, nome in enumerate(lista):
+    print(indice, nome)
+
+
+# -------------------------------------------------------------------
+idades = [15, 87, 32, 65, 56, 32, 49, 37]
+print(idades)
+print(range(len(idades)))
+print (enumerate(idades))
+print(list(range(len(idades))))  # forçando a geração de uma lista de valores
+print(list(enumerate(idades)))  #obtendo em tuplas os pares de indice e idade
+
+for valor in enumerate(idades): # retorno empacotado do método enumerated
+    print (valor)
+
+for indice, idade in enumerate(idades): # desempacotando as tuplas
+    print(indice, idade)
+
+
+#
+usuarios = [
+    ("Guilherme", 37, 1991),
+    ("Daniala", 31, 1995),
+    ("Paula", 29, 1996)
+]
+
+for nome, idade, nascimento in usuarios:  # já desempacotando
+    print(nome)
+for nome, _, _, in usuarios:  # já desempacotando ignorando os demais elementos
+    print(nome)
