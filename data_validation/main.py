994ae51df369240be9b2a02538908bc04ad19f14
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-16 18:36:23 -0300

    Phone number validation

--- data_validation/main.py
+++ data_validation/main.py
@@ -1,8 +1,20 @@
+#  Validação de CPF e CNPJ
 from cpf_cnpj import Documento
-
 
 cpf = "44452871801"
 cnpj = "28945493000151"
 doc = Documento.cria_documento(cnpj)
 doc2 = Documento.cria_documento(cpf)
 print(doc, doc2)
+
+
+# Validação de Telefone
+from telefones_br import Telefones_br
+
+
+numero = "5511972218872"
+objeto = Telefones_br(numero)
+print(objeto)
+
+
+
