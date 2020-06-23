d69ec263db4620f94043365e5adf18cac760d355
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-23 14:54:13 -0300

    Isolating objetcs creation

--- TDD/src/leilao/test_dominio_avaliador.py
+++ TDD/src/leilao/test_dominio_avaliador.py
@@ -2,22 +2,24 @@ from unittest import TestCase
 from TDD.src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
+
 class TestAvaliador(TestCase):
 
+    def cria_cenario(self):
+        self.gui = Usuario('Gui')
+        self.yuri = Usuario('Yuri')
+        self.lance_do_gui = Lance(self.gui, 100.0)
+        self.lance_do_yuri = Lance(self.yuri, 150.0)
+        self.leilao = Leilao('Celular')
+
     def test_deve_retornar_o_menor_e_o_maior_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        gui = Usuario('Gui')
-        yuri = Usuario('Yuri')
+        self.cria_cenario()
 
-        lance_do_gui = Lance(gui, 100.0)
-        lance_do_yuri = Lance(yuri, 150.0)
-
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance_do_gui)
-        leilao.lances.append(lance_do_yuri)
+        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.lances.append(self.lance_do_yuri)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
