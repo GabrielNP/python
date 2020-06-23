236e6f9df71a5d9f50a7b2fcb0b3e8271fabdf4c
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-23 18:31:32 -0300

    Adding new business rule and testing it

--- TDD/src/leilao/test_leilao.py
+++ TDD/src/leilao/test_leilao.py
@@ -21,17 +21,12 @@ class TestLeilao(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retornar_o_menor_e_o_maior_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        yuri = Usuario('Yuri')
-        lance_do_yuri = Lance(yuri, 500.0)
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_yuri)
-
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 500.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+    def test_nao_deve_permitir_propor_lance_em_ordem_decrescente(self):
+        with self.assertRaises(ValueError):
+            yuri = Usuario('Yuri')
+            lance_do_yuri = Lance(yuri, 500.0)
+            self.leilao.propoe(lance_do_yuri)
+            self.leilao.propoe(self.lance_do_gui)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
         self.leilao.propoe(self.lance_do_gui)
@@ -62,8 +57,8 @@ class TestLeilao(TestCase):
     def test_deve_permitir_propor_um_lance_caso_usuario_seja_diferente(self):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 150.0)
-        self.leilao.propoe(lance_do_yuri)
         self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
         quantidade_de_lances_recebido = len(self.leilao.lances)
         self.assertEqual(2, quantidade_de_lances_recebido)
 
