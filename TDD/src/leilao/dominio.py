70603d3381f310205229f766062e3d00035f0ddb
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-23 17:17:51 -0300

    For this project it doesn't make sense to have Avaliador Class with avalia() method.(Polymorfism)

--- TDD/src/leilao/dominio.py
+++ TDD/src/leilao/dominio.py
@@ -29,28 +29,22 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-
-    @property
-    def lances(self):
-        return self.__lances[:]
-
-    def propoe(self, lance: Lance):
-        self.__lances.append(lance)
-
-
-class Avaliador:
-
-    def __init__(self):
         self.maior_lance = sys.float_info.min
         self.menor_lance = sys.float_info.max
 
     def __str__(self):
         return f'Maior lance: {self.maior_lance}; Menor lance: {self.menor_lance}'
 
-    def avalia(self, leilao: Leilao):
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
+    @property
+    def lances(self):
+        return self.__lances[:]
+
+    def propoe(self, lance: Lance):
+        if lance.valor > self.maior_lance:
+            self.maior_lance = lance.valor
+        if lance.valor < self.menor_lance:
+            self.menor_lance = lance.valor
+        self.__lances.append(lance)
+
+
 
